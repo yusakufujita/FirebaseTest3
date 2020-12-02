408ff24887f0350074f4abf1d2c247baf60cfabc
藤田優作 <marozaemon1217@gmail.com>
2020-12-02 19:05:13 +0900

    first commit

---
+++ FirebaseTest3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseTest3
+//
+//  Created by 藤田優作 on 2020/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
