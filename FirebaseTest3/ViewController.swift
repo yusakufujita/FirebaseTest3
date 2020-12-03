dbd49982674b888b0fac74924b59063a3c6c0baa
藤田優作 <marozaemon1217@gmail.com>
2020-12-03 23:41:16 +0900

    画面をタップしたら、textfieldが下がる

--- FirebaseTest3/ViewController.swift
+++ FirebaseTest3/ViewController.swift
@@ -24,6 +24,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     
     
     @IBAction func twiiterLogin(_ sender: Any) {
